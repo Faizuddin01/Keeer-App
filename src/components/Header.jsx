33b5f36aaf1b2d7ca3088d48abf3278325fae512
Faizuddin01 <kaifaiz234@gmail.com>
2024-10-11 10:13:19 +0530

    Name update again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,10 @@ import WbIncandescentIcon from '@mui/icons-material/WbIncandescent';
 function Header() {
   return (
     <header>
-      <h1><WbIncandescentIcon /> My Own Keeper</h1>
+      <h1><WbIncandescentIcon /> My Keeper</h1>
     </header>
   );
 }
 
 export default Header;
+
